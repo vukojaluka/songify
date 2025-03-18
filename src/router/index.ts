9fa6eafc0dfb24d3e3074ceedd6057ea6d77a7fc
vukojaluka <luka.vukoja1991@gmail.com>
2025-03-18 16:41:13 +0100

    Added fake data.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import SongsView from '../views/SongsView.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
-            name: 'home',
-            component: HomeView,
+            name: 'songs',
+            component: SongsView,
             meta: {
-                title: 'Home',
+                title: 'Songs',
             },
         },
         {
